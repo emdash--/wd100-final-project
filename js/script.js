643323bfbcd85229838af1781ebe42159c829e75
emdash <emdash--@users.noreply.github.com>
2016-01-30 19:50:03 -0500

    JavaScript is getting there. Thanks, Elijah git status

--- js/script.js
+++ js/script.js
@@ -1,14 +1,44 @@
 
-window.onload = function() {
-	var breakfast = document.getElementById("breakfast");
 
-	function changeCat(newCat) {
-		// for (var i = newCat.length - 1; i >= 0; i--) {
-			breakfast.style.display = 'none';
-		// };
+/* Changing to work with class! Go! */
+window.onload = function() {
+
+	function hideSections() {
+		var recipeGet = document.getElementsByClassName('recipe');
+		for ( var i = 0; i < recipeGet.length; i++ ) {
+				recipeGet[i].style.display = 'none';
+			};
 	};
 
+	function showBreakfast() {
+		var breakfastGet = document.getElementsByClassName('breakfast');
+		for ( var i = 0; i < breakfastGet.length; i++ ) {
+				breakfastGet[i].style.display = 'inline-block';
+			};
+	};
+
+	function showSnacks() {
+		var snacksGet = document.getElementsByClassName('snack');
+		for ( var i = 0; i < snacksGet.length; i++ ) {
+				snacksGet[i].style.display = 'inline-block';
+		};
+	};
+
+	// Menu Link //
+	var breakfastFilter = document.getElementById("breakfastFilter");
+
 	document.getElementById('breakfastFilter').addEventListener("click", function() {
-		changeCat('breakfast');
+		hideSections();
+		showBreakfast();
+/*		var breakfastGet = document.getElementsByClassName('breakfast')[0];
+		breakfastGet.style.display = 'inline-block';*/
 	});
+
+	document.getElementById('snackFilter').addEventListener("click", function() {
+		hideSections();
+		showSnacks();
+/*		var snackGet = document.getElementsByClassName('snack')[0];
+		snackGet.style.display = 'inline-block';*/
+	});
+
 }
